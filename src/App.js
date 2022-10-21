a4dbc722c6475bdaf55949d48a72fe51625adf84
palazzid88 <palazzid88@gmail.com>
2022-10-21 14:33:21 -0300

    primer commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { Navbar } from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Navbar/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
