bf9ae6f42e9927748e46d33a2d8fbe19700bfa02
palazzid88 <palazzid88@gmail.com>
2022-10-22 17:14:25 -0300

    se trabaja en estilos

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import './CartWidget.css';
 
 export default function Cartwidget () {
     
     return (
-        <img src="./img/carrito.png" width="50"></img>
+        <img src="./img/carrito.png" className="img_carrito"></img>
     )
     
 }
